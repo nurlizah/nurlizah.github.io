e2a537b6c54c9a9406b747d1d1ded868725e404c
nurlizah <168153459+nurlizah@users.noreply.github.com>
2024-05-31 10:39:15 +0700

    Add files via upload

--- flappybird.js
+++ flappybird.js
@@ -49,7 +49,7 @@ window.onload = function() {
 
     //load images
     birdImg = new Image();
-    birdImg.src =("./kucheng.png")
+    birdImg.src =("./miii.png")
     birdImg.onload = function() {
         context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
     }
